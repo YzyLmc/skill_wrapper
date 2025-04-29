6923fbf180dcd8d389b6608bb575caa6afca7ec0
YzyLmc <yzy729600559@hotmail.com>
2025-04-29 05:25:16 -0400

    Fixing predicate invention,WIP

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 import re
 
 from data_structure import Skill, yaml
-from utils import GPT4, load_from_file, setup_logging, clean_logging, save_results
+from utils import GPT4, load_from_file, setup_logging, clean_logging, save_results, load_tasks
 from skill_sequence_proposing import SkillSequenceProposing
 from invent_predicate import invent_predicates
 from ai2thor_task_exec import convert_task_to_code
@@ -61,12 +61,12 @@ def invent_predicates_for_all_skill(model, lifted_pred_list, skill2operator, tas
 
 def main():
     # init env
-    env_config = load_from_file(args.env_config_fpath)
+    task_config = load_from_file(args.task_config_fpath)
 
-    log_save_path = setup_logging(args.save_dir, env_config["env"]) # configure logging
+    log_save_path = setup_logging(args.save_dir, task_config["env"]) # configure logging
 
     # main loop
-    if env_config["env"] in ["dorfl", "spot"]:
+    if task_config["env"] in ["dorfl", "spot"]:
         model = GPT4(engine=args.model)
         if args.continue_learning:
             # TODO: continue learning not implemented for the new system
@@ -76,10 +76,10 @@ def main():
         else:   
             # start from scratch
             lifted_pred_list = []
-            skill2operator = {}
+            skill2operator = {lifted_skill: None for lifted_skill in task_config['skills'].values()}
 
         # init skill sequence proposing system
-        skill_sequence_proposing = SkillSequenceProposing(env_config_fpath=args.env_config_fpath) # prompt not included but 
+        # skill_sequence_proposing = SkillSequenceProposing(task_config_fpath=args.task_config_fpath) # prompt not included but 
         
         if args.continue_learning:
             raise NotImplementedError("Continue learning not implemented yet")
@@ -88,20 +88,25 @@ def main():
             start_num = "0"
 
         # TODO: read or init if start from scratch
-        type_dict = {obj: obj_meta['types'] for obj, obj_meta in env_config['objects'].items()}
+        type_dict = {obj: obj_meta['types'] for obj, obj_meta in task_config['objects'].items()}
         tasks = {}
         grounded_predicate_truth_value_log = {}
 
         # main loop
         for i in range(int(start_num), args.num_iter):
-            # propose skill sequence and execute
-            tasks: list[Skill] = propose_and_execute(skill_sequence_proposing, tasks, lifted_pred_list, grounded_predicate_truth_value_log, skill2operator, args)
+            if not args.invent_pred_only:
+                # propose skill sequence and execute
+                tasks: list[Skill] = propose_and_execute(skill_sequence_proposing, tasks, lifted_pred_list, grounded_predicate_truth_value_log, skill2operator, args)
+            else:
+                assert args.load_fpath is not None, "must provide tasks.yaml to start predicate invention"
+                tasks = load_tasks(args.load_fpath, task_config)
             # invent predicates
             skill2operator, lifted_pred_list, grounded_predicate_truth_value_log = invent_predicates_for_all_skill(model, lifted_pred_list, skill2operator, tasks, grounded_predicate_truth_value_log, type_dict, args)
 
             logging.info(f"iteration #{i+1} is done")
-            operator_string_list = [f"Skill:{str(skill2operator)}\nOperator{str(operator_tuple[0])}\n" for lifted_skill, operator_tuple in skill2operator.items()]
-            logging.info(f'Operators learned this round:\n{"\n".join(operator_string_list)}')
+            operator_string_list = [f"Skill:{str(lifted_skill)}\nOperator{str(operator_tuple[0])}\n" for lifted_skill, operator_tuple in skill2operator.items()]
+            logging.info("Operators learned this round:")
+            logging.info('\n'.join(operator_string_list))
             save_results(skill2operator, lifted_pred_list, grounded_predicate_truth_value_log, args.save_dir)
 
             if args.step_by_step:
@@ -110,11 +115,11 @@ def main():
 
         clean_logging(log_save_path)
     else:
-        raise NotImplementedError(f"Env {env_config["env"]} has not been implemented.")
+        raise NotImplementedError(f"Env {task_config['env']} has not been implemented.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env_config_fpath", type=str, default="task_config/test.yaml", help="yaml file that store meta data of the env")
+    parser.add_argument("--task_config_fpath", type=str, default="task_config/dorfl.yaml", help="yaml file that store meta data of the env")
     parser.add_argument("--model", type=str, choices=["gpt-4o-2024-08-06", "chatgpt-4o-latest"], default="gpt-4o-2024-08-06")
     parser.add_argument("--num_iter", type=int, default=5, help="num of iter run the full refinement and proposal loop.")
     parser.add_argument("--step_by_step", action="store_true")
@@ -122,7 +127,7 @@ if __name__ == "__main__":
     parser.add_argument("--continue_learning", action='store_true')
     parser.add_argument("--load_fpath", type=str, help="provide the log file to restore from a previous checkpoint. must specify if continue learning is true")
     parser.add_argument("--save_dir", type=str, default='tasks/log', help="directory to save log files")
-    parser.add_argument("--invent_pred_only", type=bool, action="store_true", help="Read from existing data and invent predicates.")
+    parser.add_argument("--invent_pred_only", action="store_true", help="Read from existing data and invent predicates.")
     args = parser.parse_args()
 
     main()
